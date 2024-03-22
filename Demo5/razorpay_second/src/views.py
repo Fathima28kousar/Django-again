ef0bc6d7e512760fcee517fe7bebf957c4be0b25
Fathima28kousar <kousar28fathima@gmail.com>
2024-03-22 14:36:43 +0530

    tst

--- Demo5/razorpay_second/src/views.py
+++ Demo5/razorpay_second/src/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 import razorpay
 from .models import Coffee
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib import messages
 
 
 def home(request):
     if request.method == "POST":
         name = request.POST.get('name')
-        email = request.POST.get('email') 
-        amount = int(request.POST.get('amount')) * 100
+        email = request.POST.get('email')
+        amount = request.POST.get('amount') 
+
+        if not amount:
+            messages.error(request,'Please fill the field')
+            return redirect('home')
+        amount = int(amount) * 100
         
         # Initializing Razorpay client
         client = razorpay.Client(auth=('rzp_test_wucadtaz2NQLqm', 'Un2BvQcbNWU4MpjvhlF28G9W'))
@@ -55,6 +61,7 @@ def success(request):
                 'razorpay_payment_id': razorpay_payment_id,
                 'razorpay_signature': razorpay_signature
             }
+
             status = client.utility.verify_payment_signature(params_dict)
             
             # Updating payment status in the database
